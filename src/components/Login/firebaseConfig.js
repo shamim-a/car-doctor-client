9d3e5b465dac0e3e1e60f38677f902f617ae1be7
shamim-a <shamim.cse04@gmail.com>
2021-06-25 23:09:06 +0600

    Simply Done project Car Doctor

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBClIYe1v5WOB4FbXN-gqjytRAsUK4A9yk",
+  authDomain: "thecar-doctor.firebaseapp.com",
+  projectId: "thecar-doctor",
+  storageBucket: "thecar-doctor.appspot.com",
+  messagingSenderId: "891852318711",
+  appId: "1:891852318711:web:2950be03eaaa6a715270f2"
+};
+
+export default firebaseConfig;
